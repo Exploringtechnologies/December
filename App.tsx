9bb1cc7ab76040bfe4f3a4f503b187277f1be935
Exploringtechnologies <amanahuja0657@gmail.com>
2023-12-17 23:22:25 +0530

    added Fonts and Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 import DayListItem from './src/components/core/DayListItem';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
 
-const days = [...Array(24)].map((val,index) => index + 1);
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
 
+  const [fontLoaded, error]=useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_700Bold,
+  });
+
+  useEffect(() =>{
+    if(fontLoaded || error){
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, error]);
+
+
+  if(!fontLoaded && !error){
+    return null;
+  }
+
 
   return (
-
-
     <View style={styles.container}>
 
       <FlatList
@@ -17,7 +38,7 @@ export default function App() {
         contentContainerStyle={styles.content}
         numColumns={2}
         columnWrapperStyle={styles.column}
-        renderItem={({item}) => <DayListItem day={item} />}
+        renderItem={({ item }) => <DayListItem day={item} />}
       />
       <StatusBar style="auto" />
     </View>
